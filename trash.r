c239040c0c253717cf84b0fb61da2a18a66d1163
NoNameBoyy <thomasaf02@gmail.com>
2023-10-24 16:21:31 +0200

    Update trash.r

--- trash.r
+++ trash.r
@@ -1,32 +1,27 @@
-ggplot(proportions_df, aes(x = Année)) +
-  geom_line(aes(y = Prop_ZPN_en_ZGN, color = "Part des infractions enregistrées par la\nGendarmerie mais commises en Zone Police"), size = 1.5) +
-  geom_line(aes(y = Prop_ZGN_en_ZPN, color = "Part des infractions enregistrées par la Police \nmais commises en Zone Gendarmerie"), size = 1.5) +
-  labs(x = "Années", y = "Proportions (en %)", color = "") +
-  ggtitle("Évolution des proportions d'enregistrements d'infractions entre ZPN et ZGN") +
-  scale_color_manual(values = c("Part des infractions enregistrées par la\nGendarmerie mais commises en Zone Police" = "#E89E0B",
-                                "Part des infractions enregistrées par la Police \nmais commises en Zone Gendarmerie" = "#0F417A")) +
-  theme(legend.position = "bottom",
-        legend.title = element_text(size = 12)) +
-  ylim(5, 10)
+library(reshape2)
+library(ggplot2)
 
-ggplot(proportions_df, aes(x = Année)) +
-  geom_line(aes(y = Prop_ZPN_en_ZGN, color = "Part des violences sexuelles enregistrées par\nla Gendarmerie mais commises en Zone Police"), size = 1.5) +
-  geom_line(aes(y = Prop_ZGN_en_ZPN, color = "Part des violences sexuelles enregistrées par\nla Police mais commises en Zone Gendarmerie"), size = 1.5) +
-  labs(x = "Années", y = "Proportions", color = "") +
-  ggtitle("Évolution des proportions d'enregistrements d'infractions pour violences sexuelles entre ZPN et ZGN") +
-  scale_color_manual(values = c("Part des violences sexuelles enregistrées par\nla Gendarmerie mais commises en Zone Police" = "#E89E0B",
-                                "Part des violences sexuelles enregistrées par\nla Police mais commises en Zone Gendarmerie" = "#0F417A")) +
-  ylim(10, 16) +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 10))
+# Suppose que vous avez trois dataframes nommées proportions_df1, proportions_df2 et proportions_df3
 
-ggplot(proportions_df, aes(x = Année)) +
-  geom_line(aes(y = Prop_ZPN_en_ZGN, color = "Part des vols avec ou sans violence enregistrés\npar la gendarmerie mais commis en Zone Police"), size = 1.5) +
-  geom_line(aes(y = Prop_ZGN_en_ZPN, color = "Part des vols avec ou sans violence enregistrés\npar la Police mais commis en Zone Gendarmerie"), size = 1.5) +
-  labs(x = "Années", y = "Proportions", color = "") +
-  ggtitle("Évolution des proportions d'enregistrements de vols avec ou sans violence entre ZPN et ZGN") +
-  scale_color_manual(values = c("Part des vols avec ou sans violence enregistrés\npar la gendarmerie mais commis en Zone Police" = "#E89E0B",
-                                "Part des vols avec ou sans violence enregistrés\npar la Police mais commis en Zone Gendarmerie" = "#0F417A")) +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 10.5)) +
-  ylim(5, 10)
+# Rassembler les dataframes
+proportions_df1$type <- "Type 1"
+proportions_df2$type <- "Type 2"
+proportions_df3$type <- "Type 3"
+
+combined_df <- rbind(proportions_df1, proportions_df2, proportions_df3)
+
+# Utiliser la fonction melt pour remodeler le dataframe
+melted_df <- melt(combined_df, id.vars = c("Année", "type"))
+
+# Tracer le graphique combiné
+ggplot(melted_df, aes(x = Année, y = value, color = variable)) +
+  geom_line(size = 1.5) +
+  labs(x = "Années", y = "Proportions", color = "Légende") +
+  ggtitle("Titre du graphique combiné") +
+  scale_color_manual(values = c(
+    "Prop_ZPN_en_ZGN" = "#E89E0B",
+    "Prop_ZGN_en_ZPN" = "#0F417A"
+  )) +
+  theme(legend.position = "bottom", plot.title = element_text(size = 10.5)) +
+  facet_wrap(~type, ncol = 1, scales = "free_y") +
+  ylim(5, 16)
