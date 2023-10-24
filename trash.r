b1409ed5d747bc3a442aabb815de778c743b15d2
NoNameBoyy <thomasaf02@gmail.com>
2023-10-24 16:19:30 +0200

    Update trash.r

--- trash.r
+++ trash.r
@@ -1,4 +1,4 @@
-p2 <- ggplot(proportions_df, aes(x = Année)) +
+ggplot(proportions_df, aes(x = Année)) +
   geom_line(aes(y = Prop_ZPN_en_ZGN, color = "Part des infractions enregistrées par la\nGendarmerie mais commises en Zone Police"), size = 1.5) +
   geom_line(aes(y = Prop_ZGN_en_ZPN, color = "Part des infractions enregistrées par la Police \nmais commises en Zone Gendarmerie"), size = 1.5) +
   labs(x = "Années", y = "Proportions (en %)", color = "") +
@@ -9,7 +9,7 @@ p2 <- ggplot(proportions_df, aes(x = Année)) +
         legend.title = element_text(size = 12)) +
   ylim(5, 10)
 
-p2 <- ggplot(proportions_df, aes(x = Année)) +
+ggplot(proportions_df, aes(x = Année)) +
   geom_line(aes(y = Prop_ZPN_en_ZGN, color = "Part des violences sexuelles enregistrées par\nla Gendarmerie mais commises en Zone Police"), size = 1.5) +
   geom_line(aes(y = Prop_ZGN_en_ZPN, color = "Part des violences sexuelles enregistrées par\nla Police mais commises en Zone Gendarmerie"), size = 1.5) +
   labs(x = "Années", y = "Proportions", color = "") +
@@ -20,7 +20,7 @@ p2 <- ggplot(proportions_df, aes(x = Année)) +
   theme(legend.position = "bottom",
         plot.title = element_text(size = 10))
 
-p2 <- ggplot(proportions_df, aes(x = Année)) +
+ggplot(proportions_df, aes(x = Année)) +
   geom_line(aes(y = Prop_ZPN_en_ZGN, color = "Part des vols avec ou sans violence enregistrés\npar la gendarmerie mais commis en Zone Police"), size = 1.5) +
   geom_line(aes(y = Prop_ZGN_en_ZPN, color = "Part des vols avec ou sans violence enregistrés\npar la Police mais commis en Zone Gendarmerie"), size = 1.5) +
   labs(x = "Années", y = "Proportions", color = "") +
