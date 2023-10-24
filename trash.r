56aa9ae2a8ef9f9979d65c50c40241acc29408ea
Thomas Fernandes <92884823+Thomas-Fernandes@users.noreply.github.com>
2023-10-24 16:15:37 +0200

    Update trash.r

--- trash.r
+++ trash.r
@@ -1,7 +1,10 @@
-ggplot(proportions_df, aes(x = Annees)) +
-  geom_line(aes(y = Prop_ZPN_ZGN, color = "Prop_ZPN_ZGN"), size = 1.5) +
-  geom_line(aes(y = Prop_ZGN_ZPN, color = "Prop_ZGN_ZPN"), size = 1.5, linetype = "dashed") +
-  labs(x = "Années", y = "Proportions", color = "Légende") +
-  ggtitle("Évolution des proportions d'infractions entre ZPN et ZGN") +
-  scale_color_manual(values = c("Prop_ZPN_ZGN" = "#0F417A", "Prop_ZGN_ZPN" = "#E89E0B")) +
-  theme_minimal()
+p2 <- ggplot(proportions_df, aes(x = Année)) +
+  geom_line(aes(y = Prop_ZPN_en_ZGN, color = "Part des vols avec ou sans violence enregistrés\npar la gendarmerie mais commis en Zone Police"), size = 1.5) +
+  geom_line(aes(y = Prop_ZGN_en_ZPN, color = "Part des vols avec ou sans violence enregistrés\npar la Police mais commis en Zone Gendarmerie"), size = 1.5) +
+  labs(x = "Années", y = "Proportions", color = "") +
+  ggtitle("Évolution des proportions d'enregistrements de vols avec ou sans violence entre ZPN et ZGN") +
+  scale_color_manual(values = c("Part des vols avec ou sans violence enregistrés\npar la gendarmerie mais commis en Zone Police" = "#E89E0B",
+                                "Part des vols avec ou sans violence enregistrés\npar la Police mais commis en Zone Gendarmerie" = "#0F417A")) +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 10.5)) +
+  ylim(5, 10)
