f0ef8754e0203d131c3ffcedc9289d74ab76a284
NoNameBoyy <thomasaf02@gmail.com>
2023-10-30 12:28:20 +0100

    Update trash.r

--- trash.r
+++ trash.r
@@ -34,16 +34,26 @@ for(i in 1:length(bases_infractions)){
 
 create_matrix <- function(base) {
   matrice <- data.frame(
-    "0-Rural" = 0,
-    "1-Unités urbaines de 2 000 à 4 999 habitants" = 0,
-    "2-Unités urbaines de 5 000 à 9 999 habitants" = 0,
-    "3-Unités urbaines de 10 000 à 19 999 habitants" = 0,
-    "4-Unités urbaines de 20 000 à 49 999 habitants" = 0,
-    "5-Unités urbaines de 50 000 à 99 999 habitants" = 0,
-    "6-Unités urbaines de 100 000 à 199 999 habitants" = 0,
-    "7-Unités urbaines de 200 000 à 1 999 999 habitants" = 0,
-    "8-Agglomération de Paris" = 0,
-    row.names = c("0-Rural", "1-Unités urbaines de 2 000 à 4 999 habitants", "2-Unités urbaines de 5 000 à 9 999 habitants", "3-Unités urbaines de 10 000 à 19 999 habitants", "4-Unités urbaines de 20 000 à 49 999 habitants", "5-Unités urbaines de 50 000 à 99 999 habitants", "6-Unités urbaines de 100 000 à 199 999 habitants", "7-Unités urbaines de 200 000 à 1 999 999 habitants", "8-Agglomération de Paris")
+    "TUU_COMM_0" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_1" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_2" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_3" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_4" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_5" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_6" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_7" = c(0,0,0,0,0,0,0,0,0),
+    "TUU_COMM_8" = c(0,0,0,0,0,0,0,0,0),
+    row.names = c(
+      "TUU_ENR_0",
+      "TUU_ENR_1", 
+      "TUU_ENR_2", 
+      "TUU_ENR_3", 
+      "TUU_ENR_4", 
+      "TUU_ENR_5", 
+      "TUU_ENR_6", 
+      "TUU_ENR_7", 
+      "TUU_ENR_8"
+    )
   )
   counts <- table(base$TUU_COMMISSION, base$SAME)
 
