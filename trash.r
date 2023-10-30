43e5e6e947510c7831823d2a4471c08bed5f103c
Thomas Fernandes <92884823+Thomas-Fernandes@users.noreply.github.com>
2023-10-30 09:51:27 +0100

    Update trash.r

--- trash.r
+++ trash.r
@@ -18,22 +18,17 @@ melted_df <- melt(combined_df, id.vars = c("Année", "type"))
 # Code existant...
 
 # Ajouter des étiquettes aux points de données
-ggplot(melted_df, aes(x = Année, y = value, color = variable, linetype = type, label = value)) +
-  geom_line(size = 1.5) +
-  geom_text(aes(label = round(value, 2), vjust = -0.5), size = 3, show.legend = FALSE) + # Ajout des étiquettes
-  labs(x = "Années", y = "Proportions", color = "Légende", linetype = "Type") +
-  ggtitle("Titre du graphique combiné") +
+graph <- ggplot(melted_df, aes(x = Année, y = value, color = variable)) +
+  geom_line(size = 1) +
+  labs(x = "Années", y = "Proportions (en %)", color = "") +
+  ggtitle("Évolution des proportions d'enregistrements d'infractions entre Zone Police Nationale et Zone Gendarmerie Nationale") +
   scale_color_manual(values = c(
     "Prop_ZPN_en_ZGN" = "#E89E0B",
-    "Prop_ZGN_en_ZPN" = "#0F417A"
-  )) +
-  scale_linetype_manual(values = c("solid", "dashed", "dotted")) +
-  theme_minimal() +
-  theme(legend.position = "bottom",
-        plot.title = element_text(size = 14, hjust = 0.5, face = "bold"),
-        axis.title = element_text(size = 12, face = "bold"),
-        axis.text = element_text(size = 10),
-        legend.title = element_text(size = 10, face = "bold"),
-        legend.text = element_text(size = 9)) +
+    "Prop_ZGN_en_ZPN" = "#0F417A"),
+    labels = c(
+      "Prop_ZPN_en_ZGN" = "Part des infractions enregistrées par la\nGendarmerie mais commises en zone Police",
+      "Prop_ZGN_en_ZPN" = "Part des infractions enregistrées par la\nPolice mais commises en zone Gendarmerie"
+    )) +
+  theme(legend.position = "bottom", plot.title = element_text(size = 9.5)) +
   facet_wrap(~type, ncol = 1, scales = "free_y") +
   ylim(5, 16)
