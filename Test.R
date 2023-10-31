5ab7413201140e1a0ae2739e0e357d171c4474ce
NoNameBoyy <thomasaf02@gmail.com>
2023-10-31 12:34:08 +0100

    Update Test.R

--- Test.R
+++ Test.R
@@ -1,42 +1,30 @@
-for (i in 1:nrow(data)) {
-  dept_commission <- as.character(data$DEPT_COMMISSION[i])
-  dept_service <- as.character(data$DEPT_SERV[i])
-  print(dept_commission)
-  print(dept_service)
-  if(!is.na(dept_commission) && dept_commission != "" && !is.na(dept_service) && dept_service != "") {
-    if (dept_commission == dept_service) {
-      matrice_dep[dept_commission, dept_commission] <- as.numeric(matrice_dep[dept_commission, dept_commission] + 1)
-      print(matrice_dep[dept_commission, dept_commission])
-    } else {
-      if(dept_commission %in% sorted_dep && dept_service %in% sorted_dep) {
-        matrice_dep[dept_service, dept_commission] <- as.numeric(matrice_dep[dept_service, dept_commission] + 1)
-      }
-    }
+# Créer les données
+LIEU_COMMISSION <- c(1053, 1053, 1010, 1011)
+CODE_SERVICE <- c(3, 3, 4, 4)
+
+# Créer le dataframe
+df <- data.frame(LIEU_COMMISSION, CODE_SERVICE)
+
+# Créer les données
+INSEE_SIEGE_CIRCO_NOM_COM <- c(1053, 1053, 1053, 1010)
+SRV_COD_DESCR_GENERIQUE <- c(3, 3, 3, 4)
+INSEE_COMMUNE <- c(1053, 1289, 1344, 435)
+
+# Créer le dataframe
+df2 <- data.frame(INSEE_SIEGE_CIRCO_NOM_COM, SRV_COD_DESCR_GENERIQUE, INSEE_COMMUNE)
+
+for (i in 1:nrow(df)) {
+  # Initialiser une liste vide pour stocker les résultats
+  liste_communes <- c()
+  
+  # Rechercher le CODE_SERVICE dans la colonne SRV_COD_DESCR_GENERIQUE de df2
+  rows <- which(df2$SRV_COD_DESCR_GENERIQUE == df$CODE_SERVICE[i])
+  
+  # S'il y a une ou plusieurs lignes correspondantes, récupérer les noms de communes associés
+  if (length(rows) > 0) {
+    liste_communes <- df2$INSEE_COMMUNE[rows]
   }
+  
+  # Insérer la liste dans la ligne correspondante de df, à la colonne Lieu_enr
+  df$Lieu_enr[i] <- paste(liste_communes, collapse = ", ")
 }
-
-
-#La boucle est trop longue, on va l'optimiser en utilisant table
-# Initialisation de la matrice_dep
-matrice_dep <- matrix(0, nrow = length(sorted_dep), ncol = length(sorted_dep), dimnames = list(sorted_dep, sorted_dep))
-
-# Conversion en caractères des colonnes concernées
-data$DEPT_COMMISSION <- as.character(data$DEPT_COMMISSION)
-data$DEPT_SERV <- as.character(data$DEPT_SERV)
-
-# Filtrer les lignes non vides pour DEPT_COMMISSION et DEPT_SERV
-data <- data[!is.na(data$DEPT_COMMISSION) & data$DEPT_COMMISSION != "" & !is.na(data$DEPT_SERV) & data$DEPT_SERV != "", ]
-
-# Identifier les correspondances
-matching_indices <- data$DEPT_COMMISSION == data$DEPT_SERV
-matching_depts <- data$DEPT_COMMISSION[matching_indices]
-
-# Incrémenter les correspondances
-matrice_dep[matching_depts, matching_depts] <- as.numeric(matrice_dep[matching_depts, matching_depts] + 1)
-
-# Filtrer les correspondances restantes
-remaining_indices <- data$DEPT_COMMISSION %in% sorted_dep & data$DEPT_SERV %in% sorted_dep & !matching_indices
-
-# Incrémenter les correspondances restantes
-matrice_dep[as.matrix(data[remaining_indices, c("DEPT_SERV", "DEPT_COMMISSION")])] <- 
-  matrice_dep[as.matrix(data[remaining_indices, c("DEPT_SERV", "DEPT_COMMISSION")])] + 1
